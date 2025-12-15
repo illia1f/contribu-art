5473c02539e1bbd01b25d24cf8f32cd2ffefdab9
Illia Filippov <filippov.illia.ukr@gmail.com>
2025-12-15 20:49:33 +0100

    feat: add commitlint configuration and CI workflows
    - Introduced commitlint configuration to enforce commit message standards.
    - Added GitHub Actions workflows for CI to lint and build the project.
    - Integrated commitlint validation for commit messages on push and pull request events.
    - Updated package.json and package-lock.json to include commitlint dependencies.

---
+++ commitlint.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "header-max-length": [2, "always", 72],
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat",
+        "fix",
+        "docs",
+        "style",
+        "refactor",
+        "perf",
+        "test",
+        "build",
+        "ci",
+        "chore",
+        "revert",
+      ],
+    ],
+    "scope-enum": [
+      1,
+      "always",
+      ["graph", "auth", "api", "components", "utils", "config"],
+    ],
+  },
+};
+
